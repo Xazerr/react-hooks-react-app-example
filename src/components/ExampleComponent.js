6f7f56204df11d3429deace433cd90d3410670bd
jeffreyxazer <jeffreyxazer@gmail.com>
2025-04-15 00:33:24 +0300

    Assignment complete

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-// no need to modify anything in here. The second test is looking for the returned JSX from this component
 function ExampleComponent() {
   return (
     <div className="example">
